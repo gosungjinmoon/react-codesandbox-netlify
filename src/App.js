0be592e71b0cf389957567f99ee21dfb8e71e6ae
gosungjinmoon <55925545+gosungjinmoon@users.noreply.github.com>
2020-11-08 15:37:43 +0900

    We've been depolyed haaha

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
 	return (
 		<div className="App">
 			<h1>Hello CodeSandbox netlify</h1>
-			<h2>Start editing to see some magic happen!</h2>
+			<h2>Start editing ..</h2>
 		</div>
 	);
 }
